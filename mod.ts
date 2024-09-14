f93d48425354232e5c7ea3d652e1f6b13fd92ea0
mutefish0 <648262030@qq.com>
2024-09-14 21:43:18 +0800

    chunks

--- mod.ts
+++ mod.ts
@@ -5,6 +5,7 @@ type Loader = "raw" | "napi";
 interface Options {
   output?: string;
   loader?: Loader;
+  chunks?: number;
 }
 
 function bin2ts(filename: string, opts: Options) {
@@ -33,29 +34,66 @@ export default mod;
     `;
   }
 
-  const code = `
-${prefix}
-import { decodeBase64 } from "jsr:@std/encoding@1";
-const raw = decodeBase64("${b64}");
-${suffix}
-      `;
-
   let outputName = (output ?? filename).replace(/(\.[^.]+)$/, "");
   if (!output) {
     outputName += ".base64";
   }
 
-  Deno.writeTextFileSync(`${outputName}.ts`, code);
+  if (opts.chunks && opts.chunks > 1) {
+    const chunkSize = Math.ceil(buf.byteLength / opts.chunks);
+    let entryCode = `
+    ${prefix}
+    import { decodeBase64 } from "jsr:@std/encoding@1";
+    `;
+
+    for (let i = 0; i < opts.chunks; i++) {
+      const dataStr = b64.slice(i * chunkSize, (i + 1) * chunkSize);
+      const code = `
+      export default "${dataStr}";
+          `;
+      const oname = outputName + `.chunk_${i}`;
+      Deno.writeTextFileSync(`${oname}.ts`, code);
+      entryCode += `
+      import chunk${i} from "./${oname}.ts";
+      `;
+    }
+
+    entryCode += `
+    const chunks = [];
+    `;
+
+    for (let i = 0; i < opts.chunks; i++) {
+      entryCode += `
+      chunks.push(chunk${i});
+      `;
+    }
+
+    entryCode += `
+    const raw = decodeBase64(chunks.join(""));
+    ${suffix}
+    `;
+
+    Deno.writeTextFileSync(`${outputName}.ts`, entryCode);
+  } else {
+    const code = `
+    ${prefix}
+    import { decodeBase64 } from "jsr:@std/encoding@1";
+    const raw = decodeBase64("${b64}");
+    ${suffix}
+          `;
+    Deno.writeTextFileSync(`${outputName}.ts`, code);
+  }
 }
 
 if (import.meta.main) {
   const flags = parseArgs(Deno.args, {
-    string: ["o", "l"],
+    string: ["o", "l", "c"],
   });
   const filename = flags._[0] as string;
   const output = flags.o;
   const loader = flags.l as Loader;
+  const chunks = flags.c ? parseInt(flags.c) : undefined;
   if (filename) {
-    bin2ts(filename, { output, loader });
+    bin2ts(filename, { output, loader, chunks });
   }
 }
